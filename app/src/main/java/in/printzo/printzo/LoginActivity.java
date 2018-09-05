1aaaacdf71812327280d7d529526c0354e1267e6
Vinayak KS <vinayachar007@gmail.com>
2018-09-05 20:37:41 +0530

    login

--- app/src/main/java/in/printzo/printzo/LoginActivity.java
+++ app/src/main/java/in/printzo/printzo/LoginActivity.java
@@ -3,8 +3,12 @@ package in.printzo.printzo;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 
@@ -13,13 +17,30 @@ import com.firebase.ui.auth.IdpResponse;
 import com.firebase.ui.auth.ErrorCodes;
 
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 
 import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private SignInButton mGoogleBtn;
+    private GoogleSignInClient mGoogleSignInClient;
+    private GoogleApiClient mGoogleApiClient;
+    private FirebaseAuth mAuth;
     private static final int RC_SIGN_IN = 123;
     /* private PrefManager prefManager1;*/
     private  SharedPreferences.Editor reditor;
@@ -29,83 +50,78 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        SharedPreferences  sharedPreferences = getSharedPreferences("ShaPreferences", Context.MODE_PRIVATE);
+        /*SharedPreferences  sharedPreferences = getSharedPreferences("ShaPreferences", Context.MODE_PRIVATE);
         reditor=sharedPreferences.edit();
         rfirstTime =sharedPreferences.getBoolean("rfirst", true);
 
-        /*prefManager1 = new PrefManager(this);*/
-        FirebaseAuth auth = FirebaseAuth.getInstance();
-        if (auth.getCurrentUser() != null) {
-            startActivity(new Intent(this, MainActivity.class));
-            finish();
-        } else {
-            showSignInScreen();
-        }
+        mAuth = FirebaseAuth.getInstance();
+        mGoogleBtn = (SignInButton) findViewById(R.id.googlebutton);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+
     }
-    private void showSignInScreen() {
-        startActivityForResult(
-                AuthUI.getInstance()
-                        .createSignInIntentBuilder()
-                        .setAvailableProviders(
-                                Arrays.asList(
-                                        new AuthUI.IdpConfig.Builder(AuthUI.PHONE_VERIFICATION_PROVIDER).build()
-                                        /*new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()*/))
-                        .setTheme(R.style.LoginTheme)
-                        .setLogo(R.drawable.stolx)
-                        .build(),
-                RC_SIGN_IN);
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        // RC_SIGN_IN is the request code you passed into startActivityForResult(...) when starting the sign in flow.
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
-            IdpResponse response = IdpResponse.fromResultIntent(data);
-
-            // Successfully signed in
-            if (resultCode == ResultCodes.OK) {
-                //startActivity(MainActivity.createIntent(this, response));
-                if (FirebaseAuth.getInstance().getCurrentUser()!=null){
-                    if(rfirstTime){
-                        reditor.putBoolean("rfirst",false);
-                        //For commit the changes, Use either editor.commit(); or  editor.apply();.
-                        reditor.commit();
-                        startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
-                        finish();
-                        /*prefManager1.setFirstTimeLaunch(false);*/
-                    }else {
-                        startActivity(new Intent(LoginActivity.this,NavigationActivity.class));
-                    }
-
-                    return;
-                }
-            } else {
-                // Sign in failed
-                if (response == null) {
-                    // User pressed back button
-                    //showSnackbar(R.string.sign_in_cancelled);
-                    Toast.makeText(getApplicationContext(),"signin cancelled",Toast.LENGTH_LONG).show();
-                    return;
-                }
-
-                if (response.getErrorCode() == ErrorCodes.NO_NETWORK) {
-                    //showSnackbar(R.string.no_internet_connection);
-                    Toast.makeText(getApplicationContext(),"NOn internet",Toast.LENGTH_LONG).show();
-                    return;
-                }
-
-                if (response.getErrorCode() == ErrorCodes.UNKNOWN_ERROR) {
-                    //showSnackbar(R.string.unknown_error);
-                    Toast.makeText(getApplicationContext(),"Login Failed plz try again...",Toast.LENGTH_LONG).show();
-                    return;
-                }
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w("TAGF", "Google sign in failed", e);
+                // ...
             }
-
-            //showSnackbar(R.string.unknown_sign_in_response);
-            Toast.makeText(getApplicationContext(),"unknown sign in response",Toast.LENGTH_LONG).show();
         }
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d("TAG1", "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("TAG2", "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                           // updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("TAG3", "signInWithCredential:failure", task.getException());
+                            //Snackbar.make(findViewById(R.id.main_layout), "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    */}
+
+
 }
-}
+
